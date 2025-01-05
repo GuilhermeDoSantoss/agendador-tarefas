b298a8eabc661abbe2ab558d7a539d8dd9a6fff7
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-01-05 09:54:20 -0300

    adicionados metodos Get por email e por período

--- src/main/java/com/guilhermesantos/agendador/business/mapper/TarefasConverter.java
+++ src/main/java/com/guilhermesantos/agendador/business/mapper/TarefasConverter.java
@@ -3,11 +3,21 @@ package com.guilhermesantos.agendador.business.mapper;
 import com.guilhermesantos.agendador.business.DTO.TarefasDTO;
 import com.guilhermesantos.agendador.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataEvento", target = "dataEvento")
+    @Mapping(source = "dataCriacao", target = "dataCriacao")
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }
